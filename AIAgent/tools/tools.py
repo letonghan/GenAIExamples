b8cc014d634f1d103d277a764272be8750d57ff4
letonghan <letong.han@intel.com>
2024-11-01 15:53:33 +0800

    add param k for web retriever
    Signed-off-by: letonghan <letong.han@intel.com>

--- AIAgent/tools/tools.py
+++ AIAgent/tools/tools.py
@@ -16,13 +16,9 @@ def send_post_request(url, payload):
 
 def web_search_retriever(query: str) -> str:
     """Search from web for a specific query."""
-    from datetime import datetime
-
-    start_time = datetime.now()
     
     tei_embedding_endpoint = os.environ.get("TEI_EMBEDDING_ENDPOINT")
     web_retriever_endpoint = os.environ.get("WEB_RETRIEVER_ENDPOINT")
-    tei_reranking_endpoint = os.environ.get("TEI_RERANKING_ENDPOINT")
     
     # prepare embedding vector
     embedding_payload = {
@@ -35,7 +31,8 @@ def web_search_retriever(query: str) -> str:
     max_retries = 5
     web_retriever_payload = {
         "text": query,
-        "embedding": embedding_vector
+        "embedding": embedding_vector,
+        "k": 8
     }
     for i in range(max_retries):
         try:
@@ -48,6 +45,7 @@ def web_search_retriever(query: str) -> str:
         except Exception as e:
             print(f"Fail to call /v1/web_retrieval. Error: {e}. Tried {i+1} times.")
             
+    # parse web search result
     if response:
         retrieved_docs = response.json()['retrieved_docs']
         retrieved_doc_list = [doc['text'] for doc in retrieved_docs]
@@ -55,24 +53,6 @@ def web_search_retriever(query: str) -> str:
     else:
         return ""
     
-    #############################
-    # rerank the retrieved docs #
-    #############################
-    # rerank_payload = {
-    #     "query": query,
-    #     "texts": retrieved_doc_list
-    # }
-    # rerank_res = send_post_request(tei_reranking_endpoint+"/rerank", rerank_payload).json()
-    # highest_score_index = max(rerank_res, key=lambda x: x['score'])['index']
-    
-    # final_result = retrieved_doc_list[highest_score_index]
-    # print("\n======== web search ========")
-    # print(final_result)
-    
-    # elapsed_time = datetime.now() - start_time
-    # print("==================")
-    # print(f"Execution time: {elapsed_time}\n")
-    # return final_result
 
 def rag_retriever(query: str, files: str) -> str:
     """Search from files/local database for a specific query."""
