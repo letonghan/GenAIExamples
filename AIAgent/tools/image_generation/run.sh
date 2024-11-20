f04f2142985957c9c6a9d1b6ff6955ed0fc65528
letonghan <letong.han@intel.com>
2024-11-20 11:11:56 +0800

    refine tools compose
    Signed-off-by: letonghan <letong.han@intel.com>

--- AIAgent/tools/image_generation/run.sh
+++ AIAgent/tools/image_generation/run.sh
@@ -6,8 +6,9 @@ export ip_address=$(hostname -I | awk '{print $1}')
 export no_proxy=${ip_address}
 
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
-export IMAGE_MODEL="stable-diffusion-v1-5/stable-diffusion-v1-5"
+export IMAGE_MODEL="IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1"
 export HABANA_VISIBLE_DEVICES="5"
+export HF_CACHE_DIR="/home/kding1/letong/data"
 
 
 docker compose -f compose.yaml up -d
--- AIAgent/tools/web_retriever/run.sh
+++ AIAgent/tools/web_retriever/run.sh
@@ -11,6 +11,8 @@ export TEI_EMBEDDING_ENDPOINT="http://${ip_address}:3001"
 export GOOGLE_API_KEY=${GOOGLE_API_KEY}
 export GOOGLE_CSE_ID=${GOOGLE_CSE_ID}
 export LOGFLAG="true"
+export HF_CACHE_DIR="/home/kding1/letong/data"
+export HABANA_VISIBLE_DEVICES="4"
 
 
 docker compose -f compose.yaml up -d
