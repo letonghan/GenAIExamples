d35554020d4263d8edd423780341e6dc671c0252
letonghan <letong.han@intel.com>
2024-11-25 16:16:18 +0800

    refine summarize api
    Signed-off-by: letonghan <letong.han@intel.com>

--- AIAgent/ai_agent.py
+++ AIAgent/ai_agent.py
@@ -81,7 +81,7 @@ async def agent_start(input: LLMParamsDoc):
     port=service_port,
 )
 @register_statistics(names=["opea_service@ai_agent"])
-async def agent_start(input: LLMParamsDoc):
+async def agent_execute(input: LLMParamsDoc):
     if logflag:
         logger.info("[ Execute ] calling /v1/agent/execute router")
         logger.info(input)
@@ -102,7 +102,7 @@ async def agent_start(input: LLMParamsDoc):
     port=service_port,
 )
 @register_statistics(names=["opea_service@ai_agent"])
-async def agent_start(input: AgentSumDoc):
+async def agent_summarize(input: AgentSumDoc):
     if logflag:
         logger.info("[ Summarize ] calling /v1/agent/start router")
         logger.info(input)
@@ -114,49 +114,87 @@ async def agent_start(input: AgentSumDoc):
     text = " ".join(results)
     prompt = summarize_prompt_new.format(goal=goal, language=language, text=text)
 
-    # calculate input tokens
-    num_tokens = cal_tokens(prompt)
-    if num_tokens > 8191:
-        logger.info(f"llm input tokens are longer than 8191.")
-        return "LLM input tokens are longer than 8191."
-
     if logflag:
         logger.info(f"[ Summarize ] final input prompt: {prompt}")
     
-    text_generation = await llm.text_generation(
-        prompt=prompt,
-        stream=input.streaming,
-        max_new_tokens=input.max_tokens,
-        repetition_penalty=input.repetition_penalty,
-        temperature=input.temperature,
-        top_k=input.top_k,
-        top_p=input.top_p,
-    )
-    
-    if input.streaming:
-        async def stream_generator():
-            chat_response = ""
-            async for text in text_generation:
-                chat_response += text
-                lines = text.split("\n")
-                chunk_repr = "\\n".join(lines)
-                chunk_repr = chunk_repr.replace(" ", "&nbsp;")
-                if logflag:
-                    logger.info(f"[ Summarize ] chunk:{chunk_repr}")
-                # stop generate for stopper sign
-                if chunk_repr == "<|im_end|>":
-                    break
-                yield f"data: {chunk_repr}\n\n"
-            if logflag:
-                logger.info(f"[ Summarize ] stream response: {chat_response}")
-            yield "data: [DONE]\n\n"
+    framework = "vllm"
 
-        return StreamingResponse(stream_generator(), media_type="text/event-stream")
-
-    else:
+    if framework == "vllm":
+        from langchain_community.llms import VLLMOpenAI
+        llm_endpoint = os.getenv("vLLM_ENDPOINT", "http://localhost:8086")
+        model_name = os.getenv("LLM_MODEL", "Qwen/Qwen2.5-32B-Instruct")
+        vllm = VLLMOpenAI(openai_api_key="EMPTY", openai_api_base=llm_endpoint + "/v1", model_name=model_name)
+        parameters = {
+            "max_tokens": input.max_tokens,
+            "top_p": input.top_p,
+            "temperature": input.temperature,
+            "frequency_penalty": input.frequency_penalty,
+            "presence_penalty": input.presence_penalty,
+        }
         if logflag:
-            logger.info(f"[ Summarize ] text generation: {text_generation}")
-        return text_generation
+            logger.info(f"params: {parameters}")
+        
+        if input.streaming:
+            async def stream_generator():
+                chat_response = ""
+                async for text in vllm.astream(prompt, **parameters):
+                    chat_response += text
+                    if logflag:
+                        logger.info(f"[ Summarize - vllm ] chunk: {text}")
+                    lines = text.split("\n")
+                    chunk_repr = "\\n".join(lines)
+                    chunk_repr = chunk_repr.replace(" ", "&nbsp;")
+                    if chunk_repr == "<|im_end|>":
+                        break
+                    yield f"data: {chunk_repr}\n\n"
+                if logflag:
+                    logger.info(f"[ Summarize - vllm ] stream response: {chat_response}")
+                yield "data: [DONE]\n\n"
+
+            return StreamingResponse(stream_generator(), media_type="text/event-stream")
+        else:
+            response = await vllm.ainvoke(prompt, **parameters)
+            if logflag:
+                logger.info(f"[ Summarize - vllm ] text generation: {response}")
+
+            return response
+    
+    elif framework == "tgi":
+        text_generation = await llm.text_generation(
+            prompt=prompt,
+            stream=input.streaming,
+            max_new_tokens=input.max_tokens,
+            repetition_penalty=input.repetition_penalty,
+            frequency_penalty=input.frequency_penalty,
+            temperature=input.temperature,
+            top_k=input.top_k,
+            top_p=input.top_p,
+        )
+        
+        if input.streaming:
+            async def stream_generator():
+                chat_response = ""
+                async for text in text_generation:
+                    chat_response += text
+                    lines = text.split("\n")
+                    chunk_repr = "\\n".join(lines)
+                    chunk_repr = chunk_repr.replace(" ", "&nbsp;")
+                    if logflag:
+                        logger.info(f"[ Summarize - tgi ] chunk:{chunk_repr}")
+                    if chunk_repr == "<|im_end|>":
+                        break
+                    yield f"data: {chunk_repr}\n\n"
+                if logflag:
+                    logger.info(f"[ Summarize - tgi ] stream response: {chat_response}")
+                yield "data: [DONE]\n\n"
+
+            return StreamingResponse(stream_generator(), media_type="text/event-stream")
+
+        else:
+            if logflag:
+                logger.info(f"[ Summarize - tgi ] text generation: {text_generation}")
+            return text_generation
+
 
 
 if __name__ == "__main__":
